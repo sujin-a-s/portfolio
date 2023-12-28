106d27e687133b1ae4e595cf489d121659b41864
Sujin <sunilkumarsujin@gmail.com>
2023-12-28 23:41:26 +0530

    portfolio

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -88,7 +88,7 @@ function Footer() {
   return (
     <FooterContainer>
       <FooterWrapper>
-        <Logo>Kerthik </Logo>
+        <Logo>Sujin A S </Logo>
         <Nav>
           <NavLink href="#about">About</NavLink>
           <NavLink href="#skills">Skills</NavLink>
@@ -103,7 +103,7 @@ function Footer() {
           <SocialMediaIcon href={Bio.insta} target="display"><InstagramIcon /></SocialMediaIcon>
         </SocialMediaIcons>
         <Copyright>
-          &copy; 2023 Kerthik. All rights reserved.
+          &copy; 2023 Sujin. All rights reserved.
         </Copyright>
 
       </FooterWrapper>
